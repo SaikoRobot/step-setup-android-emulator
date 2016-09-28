f186c254271b77d5daeea14ea23cf74683950941
SaikoRobot <masaki.natsuki@saikorobot.com>
2016-09-28 18:54:51 +0900

    Remove timezone quotes

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ echo no | android create avd --force -n $WERCKER_SETUP_ANDROID_EMULATOR_TARGET_N
 echo "hw.keyboard=yes" >> ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME.avd/config.ini
 echo "Enabled hardware keyboard"
 cat ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME.avd/config.ini
-emulator64-arm -avd $WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME -no-audio -no-window -timezone "Asia/Tokyo" &
+emulator64-arm -avd $WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME -no-audio -no-window -timezone Asia/Tokyo &
 $WERCKER_STEP_ROOT/wait_for_emulator.sh

08428afaf2bdcb7639fad025bdec0c8af603af8e
SaikoRobot <masaki.natsuki@saikorobot.com>
2016-09-28 14:25:49 +0900

    Use emulator64-arm

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ echo no | android create avd --force -n $WERCKER_SETUP_ANDROID_EMULATOR_TARGET_N
 echo "hw.keyboard=yes" >> ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME.avd/config.ini
 echo "Enabled hardware keyboard"
 cat ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME.avd/config.ini
-emulator -avd $WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME -no-audio -no-window &
+emulator64-arm -avd $WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME -no-audio -no-window &
 $WERCKER_STEP_ROOT/wait_for_emulator.sh
